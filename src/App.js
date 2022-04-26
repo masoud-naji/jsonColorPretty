5be44719d7491b5a78440a994c3153097702fb3f
Masoud Naji <63078848+masoud-naji@users.noreply.github.com>
2022-04-26 00:40:36 -0700

    custom font size and color

--- src/App.js
+++ src/App.js
@@ -7,7 +7,20 @@ export default function App() {
     <div className="App">
       <h1>JSON pretty</h1>
       <h2>show json files pretty</h2>
-      <UseJsonPretteir MockDocument={MockDocument} />
+      <UseJsonPretteir
+        prettyjson={MockDocument}
+        preBcl="white"
+        stringCl="green"
+        numberCl="darkorange"
+        booleanCl="blue"
+        nullCl="magenta"
+        keyCl="red"
+        string_font_size="1rem"
+        number_font_size="1rem"
+        boolean_font_size="1rem"
+        null_font_size="1rem"
+        key_font_size="1.2rem"
+      />
     </div>
   );
 }
--- src/Components/useJsonPretteir.js
+++ src/Components/useJsonPretteir.js
@@ -1,5 +1,70 @@
-function UseJsonPretteir(MockDocument) {
-  function syntaxPretty(json) {
+import { useEffect } from "react";
+import "./useJsonPretteir.css";
+function UseJsonPretteir({
+  prettyjson,
+  preBcl,
+  stringCl,
+  numberCl,
+  booleanCl,
+  nullCl,
+  keyCl,
+  string_font_size,
+  number_font_size,
+  boolean_font_size,
+  null_font_size,
+  key_font_size
+}) {
+  useEffect(() => {
+    document.documentElement.style.setProperty("--preBcl", preBcl || "white");
+    document.documentElement.style.setProperty(
+      "--stringCl",
+      stringCl || "green"
+    );
+    document.documentElement.style.setProperty(
+      "--numberCl",
+      numberCl || "darkorange"
+    );
+    document.documentElement.style.setProperty(
+      "--booleanCl",
+      booleanCl || "blue"
+    );
+    document.documentElement.style.setProperty("--nullCl", nullCl || "magenta");
+    document.documentElement.style.setProperty("--keyCl", keyCl || "red");
+    document.documentElement.style.setProperty(
+      "--string_font_size",
+      string_font_size || ".8rem"
+    );
+    document.documentElement.style.setProperty(
+      "--number_font_size",
+      number_font_size || ".8rem"
+    );
+    document.documentElement.style.setProperty(
+      "--boolean_font_size",
+      boolean_font_size || ".8rem"
+    );
+    document.documentElement.style.setProperty(
+      "--null_font_size",
+      null_font_size || ".8rem"
+    );
+    document.documentElement.style.setProperty(
+      "--key_font_size",
+      key_font_size || ".8rem"
+    );
+  }, [
+    preBcl,
+    stringCl,
+    numberCl,
+    booleanCl,
+    nullCl,
+    keyCl,
+    string_font_size,
+    number_font_size,
+    boolean_font_size,
+    null_font_size,
+    key_font_size
+  ]);
+
+  function syntaxHighlight(json) {
     json = json
       .replace(/&/g, "&amp;")
       .replace(/</g, "&lt;")
@@ -23,7 +88,7 @@ function UseJsonPretteir(MockDocument) {
       }
     );
   }
-  let str = JSON.stringify(MockDocument, undefined, 4);
-  return <pre dangerouslySetInnerHTML={{ __html: syntaxPretty(str) }} />;
+  let str = JSON.stringify(prettyjson, undefined, 4);
+  return <pre dangerouslySetInnerHTML={{ __html: syntaxHighlight(str) }} />;
 }
 export default UseJsonPretteir;
